27bf70831e422ebe5bc51afde13281ff7a4f7d9c
Jena <jpantano30@gmail.com>
2024-02-24 17:35:28 -0500

    its a mess but got the, buttons all fetching

--- cooking.js
+++ cooking.js
@@ -1,56 +1,196 @@
 document.addEventListener('DOMContentLoaded', () => {
-const app_id = 'f674e30b'
-const app_key = '6560b677fd0aed4b118c0e7d7d2d3240'
-const endPoint = 'https://api.edamam.com/api/recipes/v2'
+  const app_id = 'f674e30b'
+  const app_key = '6560b677fd0aed4b118c0e7d7d2d3240'
+  const endPoint = 'https://api.edamam.com/api/recipes/v2'
 
 
 
-const modal = document.getElementById('modal')
-const bfastBtn = document.getElementById('bfast')
-const lunchBtn = document.getElementById('lunch')
-const dinnerBtn = document.getElementById('dinner')
-const dessertBtn = document.getElementById('dessert')
-const searchByKeyWordBtn = document.getElementById('byName')
-const searchByIngredientBtn = document.getElementById('byIngredient')
-const inputKeyWord = document.getElementById('inputKW')
-const inputIngredient = document.getElementById('inputIng')
-const keyWordDiv = document.getElementById('inputkw')
-const ingredDiv = document.getElementById('inputI')
-const burnBtn = document.getElementById('burnEnergy')
-const resultsContainer = document.querySelector('#results1')
-const resultsContainer2 = document.querySelector('#results2')
-const backBtn = document.querySelector('.back')
-const backBtn2 = document.querySelector('#back2')
-const dairyFBtn = document.getElementById('dairy-free')
-const glutenFBtn = document.getElementById('gluten-free')
-const ketoBtn = document.getElementById('keto')
-const kosherBtn = document.getElementById('kosher')
-const veganBtn = document.getElementById('vegan')
-const vegBtn = document.getElementById('veg')
-const lowSugBtn = document.getElementById('low-sugar')
-const noneBtn = document.getElementById('none')
+  const modal = document.getElementById('modal')
+  const bfastBtn = document.getElementById('bfast')
+  const lunchBtn = document.getElementById('lunch')
+  const dinnerBtn = document.getElementById('dinner')
+  const dessertBtn = document.getElementById('dessert')
+  const searchByKeyWordBtn = document.getElementById('byName')
+  const searchByIngredientBtn = document.getElementById('byIngredient')
+  const inputKeyWord = document.getElementById('inputKW')
+  const inputIngredient = document.getElementById('inputIng')
+  const keyWordDiv = document.getElementById('inputkw')
+  const ingredDiv = document.getElementById('inputI')
+  const ingredientForm = document.getElementById('iForm')
+  const burnBtn = document.getElementById('burnEnergy')
+  const resultsContainer = document.querySelector('#results1')
+  const resultsContainer2 = document.querySelector('#results2')
+  const backBtn = document.querySelector('.back')
+  const backBtn2 = document.querySelector('#back2')
+  const searchByBtns = document.getElementById('idliketoBTNS')
+  const backBtn3 = document.querySelector('#back3')
+  const backBtn4 = document.querySelector('#back4')
+
+
+  const questionOne = document.getElementById('modal-1')
+  const questionTwo = document.getElementById('modal-2')
+
+
+  let mealType = ''
+  let dessertBtnPressed = false
+
+  //carousel 
+  const next = document.querySelector('.nxt')
+  const previous = document.querySelector('.prev')
+  const images = document.getElementsByClassName('images')
+
+  let currentImgIndex = 0
+  let previousImgIndex = 0
+
+
+
+  let diet = ''
+
+  
+  const dairyFBtn = document.getElementById('dairy-free')
+  const glutenFBtn = document.getElementById('gluten-free')
+  const ketoBtn = document.getElementById('keto')
+  const kosherBtn = document.getElementById('kosher')
+  const veganBtn = document.getElementById('vegan')
+  const vegBtn = document.getElementById('veg')
+  const lowSugBtn = document.getElementById('low-sugar')
+  const noneBtn = document.getElementById('none')
+
+  
+
+  const resultsModalInput = document.getElementById('resultsFromModalInput')
+
+  const fetchForHealth = `${endPoint}?type=public&app_id=${app_id}&app_key=${app_key}&mealType=${mealType}&health=${diet}&random=true`
+  const dessertFetchForH = `${endPoint}?type=public&app_id=${app_id}&app_key=${app_key}&dishType=Desserts&health=${diet}&random=true`
+
+
+  const fetchFunctionHealth = (fetchFor) => {
+    fetch(fetchFor)
+    .then((data) => {
+      console.log(data)
+      return data.json()
+    },
+    (err) => {
+      console.log("Error: ", err)
+    }
+    ).then(
+      (json) => {
+        console.log("JSON DATA: ", json)
+    
+
+        if(json.hits && json.hits.length > 0){
+          json.hits.forEach((hit) => {
+            const recipeTitle = hit.recipe.label
+            const recipeURL = hit.recipe.url
+            const recipeImg = hit.recipe.image 
+            console.log(recipeTitle, recipeURL, recipeImg)
+            modal.style.display = 'none'
+            searchByBtns.style.display = 'none'
+            burnBtn.style.display = 'none'
+            backBtn3.style.display = 'flex'
+            const resultItem = document.createElement('div')
+            resultItem.classList.add('recipe-result')
+            resultItem.innerHTML = `
+            <img src="${recipeImg}" alt="${recipeTitle}" class="recipe-image">
+            <p class="recipe-title"><a href="${recipeURL}" target="_blank">Click here to get this recipe: 
+            ${recipeTitle}</a></p>`
+            resultsModalInput.appendChild(resultItem)
+            resultsModalInput.style.display = 'block'
+          })
+        } else {
+          console.log(resultsModalInput)
+          resultsModalInput.style.display = 'none'
+          const errorMessage = document.createElement('h1')
+
+          errorMessage.innerText = "Key Word Not Found"
+          document.querySelector('#two').append(errorMessage)
+        }
+      })
+      .catch((err) => {
+        console.log('Error: ', err)
+      })
+  }
 
 
 
 
+  searchByKeyWordBtn.addEventListener('click', () => {
+    modal.style.display = 'none'
+    searchByIngredientBtn.style.display = 'none'
+    searchByKeyWordBtn.style.display = 'none'
+    keyWordDiv.style.display = 'flex'
+    keyWordDiv.style.flexDirection = 'column'
+    burnBtn.style.display = 'none'
 
-searchByKeyWordBtn.addEventListener('click', () => {
-  modal.style.display = 'none'
-  searchByIngredientBtn.style.display = 'none'
-  searchByKeyWordBtn.style.display = 'none'
-  keyWordDiv.style.display = 'flex'
-  keyWordDiv.style.flexDirection = 'column'
-  burnBtn.style.display = 'none'
+    resultsContainer.innerText = ''
+    // document.querySelector('#one').innerText = ''
+  })
+  const KeyWordForm = document.getElementById('kwForm')
+  KeyWordForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      const valueKW = inputKeyWord.value
 
-  resultsContainer.innerText = ''
-  // document.querySelector('#one').innerText = ''
-})
+      fetch(`${endPoint}?type=any&q=${valueKW}&app_id=${app_id}&app_key=${app_key}&random=true`).then(
+        (data) => {
+          console.log(data)
+          return data.json()
+        },
+        (err) => {
+          console.log("Error: ", err)
+        }
+      ).then(
+        (json) => {
+          console.log("JSON DATA: ", json)
 
- document.getElementById('kwForm').addEventListener('submit', (e) => {
+          if(json.hits && json.hits.length > 0){
+            //clear last results 
+            resultsContainer.innerText = ''
+            // clearing error message is not working 
+            // errorMessage.innerText = ''
+
+            json.hits.forEach((hit) => {
+              const recipeTitle = hit.recipe.label
+              const recipeURL = hit.recipe.url
+              const recipeImg = hit.recipe.image
+
+              const resultItem = document.createElement('div')
+              resultItem.classList.add('recipe-result')
+              resultItem.innerHTML = `
+              <img src="${recipeImg}" alt="${recipeTitle}" class="recipe-image">
+              <p class="recipe-title"><a href="${recipeURL}" target="_blank">${recipeTitle}</a></p>`
+              resultsContainer.appendChild(resultItem)
+            })
+          } else {
+            console.log(resultsContainer)
+            resultsContainer.style.display = 'none'
+            const errorMessage = document.createElement('h1')
+            errorMessage.innerText = "Key Word Not Found"
+            document.querySelector('#one').append(errorMessage)
+          }
+        })
+        .catch((err) => {
+          console.log('Error: ', err)
+        })
+    })
+
+
+  searchByIngredientBtn.addEventListener('click', () => {
+    modal.style.display = 'none'
+    searchByIngredientBtn.style.display = 'none'
+    searchByKeyWordBtn.style.display = 'none'
+    ingredDiv.style.display = 'flex'
+    ingredDiv.style.flexDirection = 'column'
+    ingredDiv.style.gridArea = 'modal'
+    burnBtn.style.display = 'none'
+    resultsContainer2.innerText = '';
+    document.querySelector('#two').innerText = '';
+  })
+
+  ingredientForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const valueKW = inputKeyWord.value
+    const valueIngred = inputIngredient.value
 
-    fetch(`${endPoint}?type=any&q=${valueKW}&app_id=${app_id}&app_key=${app_key}`).then(
+    fetch(`${endPoint}?type=any&q=${valueIngred}&app_id=${app_id}&app_key=${app_key}&random=true`).then(
       (data) => {
         console.log(data)
         return data.json()
@@ -64,11 +204,12 @@ searchByKeyWordBtn.addEventListener('click', () => {
 
         if(json.hits && json.hits.length > 0){
           //clear last results 
-          resultsContainer.innerText = ''
-          // clearing error message is not working 
+          resultsContainer2.innerText = ''
           // errorMessage.innerText = ''
 
           json.hits.forEach((hit) => {
+            //cant get results to show up in the modal grid area 
+            // ingredientForm.style.gridArea = 'modal'
             const recipeTitle = hit.recipe.label
             const recipeURL = hit.recipe.url
             const recipeImg = hit.recipe.image
@@ -78,14 +219,15 @@ searchByKeyWordBtn.addEventListener('click', () => {
             resultItem.innerHTML = `
             <img src="${recipeImg}" alt="${recipeTitle}" class="recipe-image">
             <p class="recipe-title"><a href="${recipeURL}" target="_blank">${recipeTitle}</a></p>`
-            resultsContainer.appendChild(resultItem)
+            resultsContainer2.appendChild(resultItem)
           })
         } else {
-          console.log(document.getElementById('results1'))
-          resultsContainer.style.display = 'none'
+          console.log(document.getElementById('results2'))
+          resultsContainer2.style.display = 'none'
           const errorMessage = document.createElement('h1')
+
           errorMessage.innerText = "Key Word Not Found"
-          document.querySelector('#one').append(errorMessage)
+          document.querySelector('#two').append(errorMessage)
         }
       })
       .catch((err) => {
@@ -93,101 +235,46 @@ searchByKeyWordBtn.addEventListener('click', () => {
       })
   })
 
+  const back = () => {
+    modal.style.display = 'flex'
+    searchByBtns.style.display = 'flex'
+    searchByIngredientBtn.style.display = 'flex'
+    searchByKeyWordBtn.style.display = 'flex'
+    burnBtn.style.display = 'block'
+    keyWordDiv.style.display = 'none'
+    ingredDiv.style.display = 'none'
+    // resultsModalInput.style.display = 'none'
+    resultsContainer.style.display = 'block'
+    resultsContainer2.style.display = 'block'
+    questionOne.style.display = 'flex'
+    questionTwo.style.display = 'none'
+    backBtn3.style.display = 'none'
+  
+    // Clear previous results
+    resultsContainer.innerHTML = ''
+    resultsContainer2.innerHTML = ''
+    resultsModalInput.innerText = ''
 
-searchByIngredientBtn.addEventListener('click', () => {
-  modal.style.display = 'none'
-  searchByIngredientBtn.style.display = 'none'
-  searchByKeyWordBtn.style.display = 'none'
-  ingredDiv.style.display = 'flex'
-  ingredDiv.style.flexDirection = 'column'
-  burnBtn.style.display = 'none'
-  resultsContainer2.innerText = '';
-  document.querySelector('#two').innerText = '';
-})
+    // Remove error messages
+    const errorMessagesOne = document.querySelectorAll('#one .error-message')
+    errorMessagesOne.forEach((errorMessage) => errorMessage.remove())
 
-document.getElementById('iForm').addEventListener('submit', (e) => {
-  e.preventDefault()
-  const valueIngred = inputIngredient.value
+    const errorMessagesTwo = document.querySelectorAll('#two .error-message')
+    errorMessagesTwo.forEach((errorMessage) => errorMessage.remove())
+  }
 
-  fetch(`${endPoint}?type=any&q=${valueIngred}&app_id=${app_id}&app_key=${app_key}`).then(
-    (data) => {
-      console.log(data)
-      return data.json()
-    },
-    (err) => {
-      console.log("Error: ", err)
-    }
-  ).then(
-    (json) => {
-      console.log("JSON DATA: ", json)
+  backBtn.addEventListener('click', (back))
+  backBtn2.addEventListener('click', (back))
+  backBtn3.addEventListener('click', (back))
+  backBtn4.addEventListener('click', (back))
 
-      if(json.hits && json.hits.length > 0){
-        //clear last results 
-        resultsContainer2.innerText = ''
-        // errorMessage.innerText = ''
-
-        json.hits.forEach((hit) => {
-          const recipeTitle = hit.recipe.label
-          const recipeURL = hit.recipe.url
-          const recipeImg = hit.recipe.image
-
-          const resultItem = document.createElement('div')
-          resultItem.classList.add('recipe-result')
-          resultItem.innerHTML = `
-          <img src="${recipeImg}" alt="${recipeTitle}" class="recipe-image">
-          <p class="recipe-title"><a href="${recipeURL}" target="_blank">${recipeTitle}</a></p>`
-          resultsContainer2.appendChild(resultItem)
-        })
-      } else {
-        console.log(document.getElementById('results2'))
-        resultsContainer2.style.display = 'none'
-        const errorMessage = document.createElement('h1')
-
-        errorMessage.innerText = "Key Word Not Found"
-        document.querySelector('#two').append(errorMessage)
-      }
-    })
-    .catch((err) => {
-      console.log('Error: ', err)
-    })
-})
-
-const back = () => {
-  modal.style.display = 'flex'
-  searchByIngredientBtn.style.display = 'flex'
-  searchByKeyWordBtn.style.display = 'flex'
-  burnBtn.style.display = 'block'
-  keyWordDiv.style.display = 'none'
-  ingredDiv.style.display = 'none'
-  resultsContainer.style.display = 'block'
-  resultsContainer2.style.display = 'block'
-
-  // Clear previous results
-  resultsContainer.innerHTML = ''
-  resultsContainer2.innerHTML = ''
-
-  // Remove error messages
-  const errorMessagesOne = document.querySelectorAll('#one .error-message')
-  errorMessagesOne.forEach((errorMessage) => errorMessage.remove())
-
-  const errorMessagesTwo = document.querySelectorAll('#two .error-message')
-  errorMessagesTwo.forEach((errorMessage) => errorMessage.remove())
-}
-
-backBtn.addEventListener('click', (back))
-backBtn2.addEventListener('click', (back))
 
 
 
 
 
   // carousel 
-  const next = document.querySelector('.nxt')
-  const previous = document.querySelector('.prev')
-  const images = document.getElementsByClassName('images')
 
-  let currentImgIndex = 0
-  let previousImgIndex = 0
 
   next.addEventListener('click', () => {
       previousImgIndex = currentImgIndex
@@ -210,81 +297,107 @@ backBtn2.addEventListener('click', (back))
       images[currentImgIndex].style.display = 'block'
   })
 
-const q2 = () => {
-  modal.innerHTML = `<div class="modal-container" id="modal">
-  <div class="modal-textbox">
-    <h1>Do you have any dietary restrictions?</h1>
-    <div id="health"> 
-      <button class="diet" id="dairy-free">Dairy Free</button>
-      <button class="diet" id="gluten-free">Gluten Free</button>
-      <button class="diet" id="keto">Keto</button>
-      <button class="diet" id="kosher">Kosher</button>
-      <button class="diet" id="vegan">Vegan</button>
-      <button class="diet" id="veg">Vegetarian</button>
-      <button class="diet" id="low-sugar">Low Sugar</button>
-      <button class="diet" id="none">None</button>
-    </div>
-  </div>
-</div>`
-}
+
+  const q2 = () => {
+      questionOne.style.display = 'none'
+      questionTwo.style.display = 'flex'
+  }
 
 
-
-bfastBtn.addEventListener('click', () => {
-  fetch('')
-  .then(res => res.json())
-  .then(data => console.log(data))
-  q2()
   
-  })
-lunchBtn.addEventListener('click', () => {
-  q2()
+  bfastBtn.addEventListener('click', () => {
+    mealType = 'Breakfast'
+    q2()
+    
+    })
+  lunchBtn.addEventListener('click', () => {
+    mealType = 'Lunch'
+    q2()
 
-  })
-dinnerBtn.addEventListener('click', () => {
-  q2()
+    })
+  dinnerBtn.addEventListener('click', () => {
 
+    mealType = 'dinner'
+    q2()
   })
-dessertBtn.addEventListener('click', () => {
-  q2()
-
+  dessertBtn.addEventListener('click', () => {
+    dessertBtnPressed = true
+    dishType = 'dessert'
+    q2()
   })
 
-dairyFBtn.addEventListener('click', () => {
 
+
+    dairyFBtn.addEventListener('click', () => {
+    console.log('Dairy Free Button Clicked')
+    // backBtn3.style.display = 'flex'
+    // console.log(backBtn3.style.display)
+
+    diet = 'dairy-free'
+    if(dessertBtnPressed){
+      fetchFunctionHealth(dessertFetchForH)
+    } else { 
+    fetchFunctionHealth(fetchForHealth)
+    }
+  })
+ 
+
+
+
+  glutenFBtn.addEventListener('click', () => {
+    diet = 'gluten-free'
+    if(dessertBtnPressed){
+        fetchFunctionHealth(dessertFetchForH)
+      } else { 
+        fetchFunctionHealth(fetchForHealth)
+      }
+  })
+  ketoBtn.addEventListener('click', () => {
+    diet = 'keto-friendly'
+    if(dessertBtnPressed){
+      fetchFunctionHealth(dessertFetchForH)
+    } else { 
+    fetchFunctionHealth(fetchForHealth)
+    }
+  })
+  kosherBtn.addEventListener('click', () => {
+    diet = 'kosher'
+    if(dessertBtnPressed){
+      fetchFunctionHealth(dessertFetchForH)
+    } else { 
+    fetchFunctionHealth(fetchForHealth)
+    }
+  })
+  veganBtn.addEventListener('click', () => {
+    diet = 'vegan'
+    if(dessertBtnPressed){
+      fetchFunctionHealth(dessertFetchForH)
+    } else { 
+    fetchFunctionHealth(fetchForHealth)
+    }
+  })
+  vegBtn.addEventListener('click', () => {
+    diet = 'vegetarian'
+    if(dessertBtnPressed){
+      fetchFunctionHealth(dessertFetchForH)
+    } else { 
+    fetchFunctionHealth(fetchForHealth)
+    }
+  })
+  lowSugBtn.addEventListener('click', () => {
+    diet = 'low-sugar'
+    if(dessertBtnPressed){
+      fetchFunctionHealth(dessertFetchForH)
+    } else { 
+    fetchFunctionHealth(fetchForHealth)
+    }
+  })
+
+  noneBtn.addEventListener('click', () => {
+    if(dessertBtnPressed){
+      fetchFunctionHealth('https://api.edamam.com/api/recipes/v2?type=public&app_id=f674e30b&app_key=6560b677fd0aed4b118c0e7d7d2d3240&dishType=Desserts&random=true')
+    } else {
+      fetchFunctionHealth(`${endPoint}?type=any&app_id=${app_id}&app_key=${app_key}&mealType=${mealType}&random=true`)
+    }
+  })
 })
-
-glutenFBtn.addEventListener('click', () => {
-  
-})
-ketoBtn.addEventListener('click', () => {
-  
-})
-kosherBtn.addEventListener('click', () => {
-  
-})
-veganBtn.addEventListener('click', () => {
-  
-})
-vegBtn.addEventListener('click', () => {
-  
-})
-lowSugBtn.addEventListener('click', () => {
-  
-})
-noneBtn.addEventListener('click', () => {
-  
-})
-
-
-
-  // modal.style.display = 'none'
-
-
-
-
-
-})
-
-
-
